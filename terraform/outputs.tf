074f835d8d0a1160fd779bcd1ce6b964678be803
this guy <jusufusofine@gmail.com>
2022-06-07 16:25:09 +0200

    add sensitiv true to output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,17 +1,20 @@
 output "client_key" {
+  sensitive = true
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.client_key
 }
 
 output "client_certificate" {
+  sensitive = true
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.client_certificate
 }
 
 output "cluster_ca_certificate" {
+  sensitive = true
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.cluster_ca_certificate
 }
 
 output "cluster_username" {
-  sensitive = false
+  sensitive = true
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.username
 }
 
